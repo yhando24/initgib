1f3ff455aa6e08bdd4e233521b7ead71665fc281
yhando24 <youcef.handoura@gmail.com>
2018-11-12 12:11:31 +0100

    1er test git maven

---
+++ commons/src/main/java/utils/isNullOrEmpty.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class isNullOrEmpty {
+
+
+	public static boolean isNullOrEmpty(String value) {
+		return value == null || value.isEmpty();
+	}
+}
---
+++ my-first-project/src/main/java/fr/dta/my_first_project/App.java
@@ -0,0 +1,13 @@
+package fr.dta.my_first_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
